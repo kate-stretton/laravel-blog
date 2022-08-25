833ad29493a6502e1677aef7eed7d18fa7be672e
= <=>
2022-08-25 12:54:21 +1200

    Add Post class

---
+++ app/Models/Post.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+class Post
+{
+  public static function find($slug)
+  {
+    if (!file_exists($path = resource_path("/../resources/posts/{$slug}.html"))){
+      throw new ModelNotFoundException();
+    }
+    
+    return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+  }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,11 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug){
-  if (! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")){
-    return redirect('/');
-  }
-
-  $post = cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
-
-  return view ('post', ['post'=> $post]);
+  return view('post', [
+    'post' => Post::find($slug)
+  ]);
 })->where('post', '[A-z_\-]+');
