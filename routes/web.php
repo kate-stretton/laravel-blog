393d3f8f865e9e14ce18ce429279d20e9f58c6de
= <=>
2022-08-25 12:14:32 +1200

    Add redirect for files that dont exist

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,15 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug){
-  $post = file_get_contents(__DIR__ . "/../resources/posts/{$slug}.html");
+  $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+  if (! file_exists($path)){
+    return redirect('/');
+    //abort(404);
+  }
+
+  $post = file_get_contents($path);
+  
   return view ('post', [
     'post'=> $post
   ]);
