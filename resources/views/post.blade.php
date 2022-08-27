323b9590a7dbb6801fea82d0e290021cd81dcc74
= <=>
2022-08-27 14:45:34 +1200

    Make use of layout file

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,12 @@
-<!doctype html>
+@extends('layout')
 
-<title>My Blog</title>
-<link rel='stylesheet' href='/app.css'>
-
-<body>
+@section('content')
   <article>
-    <h1>{!!$post->title!!}</h1>
-    <div>
-      {!!$post->body!!}
-    </div>
-  </article>
+      <h1>{!!$post->title!!}</h1>
+      <div>
+        {!!$post->body!!}
+      </div>
+    </article>
+
   <a href='/'>Go back</a>
-</body>
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,17 @@
-<!doctype html>
+@extends ('layout')
 
-<title>My Blog</title>
-<link rel='stylesheet' href='/app.css'>
-
-<body>
+@section('content')
   @foreach ($posts as $post)
-      <article class="{{$loop->even ? 'mb-6' : ''}}">
-        <h1>
-          <a href="/posts/{{$post->slug}}">
-          {{$post->title}}
-          </a>
-        </h1>
-        
-        <div>
-          {{$post->excerpt}}
-        </div>
-      </article>
-    @endforeach
-</body>
-
-
+        <article class="{{$loop->even ? 'mb-6' : ''}}">
+          <h1>
+            <a href="/posts/{{$post->slug}}">
+            {{$post->title}}
+            </a>
+          </h1>
+          
+          <div>
+            {{$post->excerpt}}
+          </div>
+        </article>
+      @endforeach
+@endsection
