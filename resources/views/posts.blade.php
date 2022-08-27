316acb07ae5e9797b7ea2aa45b6096fdfd5473cc
= <=>
2022-08-27 14:52:15 +1200

    Refactor to include blade component

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,17 +1,15 @@
-@extends ('layout')
-
-@section('content')
+<x-layout>
   @foreach ($posts as $post)
-        <article class="{{$loop->even ? 'mb-6' : ''}}">
-          <h1>
-            <a href="/posts/{{$post->slug}}">
-            {{$post->title}}
-            </a>
-          </h1>
+    <article class="{{$loop->even ? 'mb-6' : ''}}">
+      <h1>
+        <a href="/posts/{{$post->slug}}">
+          {{$post->title}}
+        </a>
+      </h1>
           
-          <div>
-            {{$post->excerpt}}
-          </div>
-        </article>
-      @endforeach
-@endsection
+      <div>
+        {{$post->excerpt}}
+      </div>
+    </article>
+  @endforeach
+</x-layout>
